d4787f088ed1e6bf2eaccd10bf54ea52c84659c7
Vlad Spreys <vlad@spreys.com>
2015-04-06 17:30:56 +1200

    Initial commit

---
+++ Virtual_Tourist/Virtual Tourist/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Vlad Spreys on 6/04/15.
+//  Copyright (c) 2015 Spreys.com. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
