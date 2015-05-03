c128e0373a5329dba50edf1e70298097ba6669d7
Vlad Spreys <vlad@spreys.com>
2015-05-03 21:01:58 +1200

    Added functionality to the AlbumViewController

--- Virtual_Tourist/Virtual Tourist/View Controllers/AlbumViewController.swift
+++ Virtual_Tourist/Virtual Tourist/View Controllers/AlbumViewController.swift
@@ -7,10 +7,15 @@ import Foundation
 import UIKit
 import MapKit
 
-class AlbumViewController : UIViewController {
-    @IBOutlet var mapView : MKMapView!
+class AlbumViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet weak var loadingOverlay: UIView!
+    var btnNewCollection: UIButton?
     
-    var annotation : MKAnnotation?
+    var annotation: MKAnnotation?
+    var photoUrls: [String]?
+    var photosCache: [UIImage?]?
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -20,13 +25,103 @@ class AlbumViewController : UIViewController {
         
         navigationController?.navigationBarHidden = false
         
+        retrieveNewCollection()
+    }
+    
+    private func retrieveNewCollection() {
+        photoUrls = nil
+        photosCache = nil
+        loadingOverlay.hidden = false
+        
         flickrApiClient.getPhotosForCoorinate(annotation!.coordinate, completionHandler:{
             (urls:[String]) in
+            
+            //Hide loading overlay with animation
+            UIView.transitionWithView(self.loadingOverlay, duration: NSTimeInterval(0.4), options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {}, completion: {(finished: Bool) -> () in })
+            self.loadingOverlay.hidden = true
+            
+            //Update model
+            self.photoUrls = urls
+            self.photosCache = [UIImage?](count:urls.count, repeatedValue: nil)
+            
+            //Reload collection view
+            self.collectionView.reloadData()
         })
     }
     
     private var flickrApiClient : FlickrApiClient {
         var delegate = UIApplication.sharedApplication().delegate as! AppDelegate
         return delegate.flickrApiClient
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if photoUrls == nil {
+            return 0
+        } else {
+            return photoUrls!.count
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        
+        var newCollection = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "footer", forIndexPath: indexPath) as! UICollectionReusableView
+        
+        self.btnNewCollection = newCollection.subviews[0] as? UIButton
+        
+        return newCollection
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        //Deque cell
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("photoCell", forIndexPath: indexPath) as! PhotoCell
+        
+        //Check the cache
+        if let imageFromCache = photosCache![indexPath.item] {
+            cell.imageView.image = imageFromCache
+            cell.loadingOverlay.hidden = true
+        } else {
+            //Asyncrhoniously load image from URL:
+            var imageUrl = NSURL(string: photoUrls![indexPath.item])
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {
+                var image = UIImage(data: NSData(contentsOfURL: imageUrl!)!)
+                
+                //update cache
+                self.photosCache![indexPath.item] = image
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    
+                    //Check if this is still the right cell and not a re-used one
+                    if let updateCell = collectionView.cellForItemAtIndexPath(indexPath) as? PhotoCell {
+                        //Display loaded image
+                        updateCell.imageView.image = image
+                        
+                        //Hide loading overla with animation
+                        UIView.transitionWithView(cell.loadingOverlay, duration: NSTimeInterval(0.4), options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {}, completion: {(finished: Bool) -> () in })
+                        updateCell.loadingOverlay.hidden = true
+                        
+                        //Enable "New Collection" button if all of the images have been loaded
+                        self.btnNewCollection!.enabled = self.allImagesLoaded
+                    }
+                })
+            })
+        }
+        
+        return cell
+    }
+    
+    private var allImagesLoaded: Bool {
+        var loaded = true
+        for (var i = 0; i < photosCache!.count; i++){
+            if photosCache![i] == nil {
+                loaded = false
+            }
+        }
+        
+        return loaded
+    }
+    
+    @IBAction func newCollection() {
+        retrieveNewCollection()
     }
 }
---
+++ Virtual_Tourist/Virtual Tourist/Views/PhotoCell.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoCell.swift
+//  Virtual Tourist
+//
+//  Created by Vlad Spreys on 3/05/15.
+//  Copyright (c) 2015 Spreys.com. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var loadingOverlay: UIActivityIndicatorView!
+    
+    override func prepareForReuse() {
+        imageView.image = nil
+        loadingOverlay.hidden = false
+    }
+}
